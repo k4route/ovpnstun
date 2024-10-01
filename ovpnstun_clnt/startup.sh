ff39a73a73e79a7372ad6bdd8638323abf897824
Fedor Chernyshev <k4route@gmail.com>
2024-10-01 20:50:53 +0000

    full client config

--- ovpnstun_clnt/startup.sh
+++ ovpnstun_clnt/startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#add stunnel/openvpn server IP as a /32 route to keep it once ovpn replace DGW with its own
+route add -host 192.168.2.30 gw 172.17.0.1
+
 /usr/sbin/iptables -A FORWARD -i eth0 -o eth0 -j DROP
 /usr/sbin/iptables -t nat -I POSTROUTING -s 192.168.2.0/24 -o tun0 -j MASQUERADE
 /usr/bin/stunnel4 /etc/stunnel/stunnel.conf &
