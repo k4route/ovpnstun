9bd2c7e665fe0402001cb3020b402ad15ee30e8e
Fedor Chernyshev <k4route@gmail.com>
2024-10-02 09:54:50 +0000

    cnt client is ready but can not point all the traffic from laptop to it

--- ovpnstun_clnt/startup.sh
+++ ovpnstun_clnt/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #add stunnel/openvpn server IP as a /32 route to keep it once ovpn replace DGW with its own
-route add -host 192.168.2.30 gw 172.17.0.1
+route add -host 192.168.3.153 gw 192.168.2.1
 
 /usr/sbin/iptables -A FORWARD -i eth0 -o eth0 -j DROP
 /usr/sbin/iptables -t nat -I POSTROUTING -s 192.168.2.0/24 -o tun0 -j MASQUERADE
